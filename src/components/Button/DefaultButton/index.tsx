bbee28906b56f33695ea99493bfce3134e2ee1ed
John Zenith <john.zenith.me@gmail.com>
2022-03-04 10:14:52 +0100

    default button type fixed

--- src/components/Button/DefaultButton/index.tsx
+++ src/components/Button/DefaultButton/index.tsx
@@ -15,7 +15,7 @@ const DefaultButton: React.FC<Props> = props => {
   const renderButton = (
     <button
       onClick={props?.onClick}
-      aria-label={props?.ariaLabel || props.label}
+      aria-label={(props?.ariaLabel || props.label) as string}
       className={clsx('mt-4 w-full max-w-[389px] flex min-h-[30px] items-center font-sans font-medium text-sm text-textColorDark justify-center border border-solid border-borderColorDark dark:border-borderColorLight dark:hover:border-borderColorHover bg-buttonBackground hover:bg-buttonBackgroundHover transition-colors rounded-[6px]', props?.className || '')}
     >
       {props.label}
